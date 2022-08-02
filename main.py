e518585ac48801b3859639471740da354f3424c2
Jmanuel2099 <jmanuel2099@gmail.com>
2022-08-01 22:18:31 -0500

    funcinalidad de calcular el angular de refraccion

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from turtle import*
 import math
+import turtle
 
 
-def paint():
+def draw():
     title('Ensayo')
+    bgcolor('gray')
+    setup(1000, 600, 0, 0)
+
+def draw_zone_one():
+    setworldcoordinates(500, 300, 300, 0)
+    fillcolor('blue')
+    begin_fill()
+    for i in range(4):
+        forward(100)
+        right(90)
+    end_fill()
 
 
 def calculate_angle_refraction(n_a, n_b, angle_a):
-
     x = (n_a/n_b) * math.sin(degrees_to_radians(angle_a))
     print('x ->', x)
     return radians_to_degrees(math.asin(x))
 
-
 def degrees_to_radians(angle):
     return ((angle * math.pi) / 180)
-
 
 def radians_to_degrees(angle):
     return ((angle * 180) / math.pi)
 
 
 if __name__ == '__main__':
-    print(calculate_angle_refraction(1.33, 1.52, 60))
+    draw()
+    draw_zone_one()
+    mainloop()
 
