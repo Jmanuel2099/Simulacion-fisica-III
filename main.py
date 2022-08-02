d2d210e2f5cbe34e32e20726d26bf523e3389d5f
Jmanuel2099 <jmanuel2099@gmail.com>
2022-08-01 23:49:30 -0500

    calculate angle of refraction

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
-from turtle import*
-import math
-import turtle
+from refraction_reflection import refraction_reflection
+import pygame
 
-
-def draw():
-    title('Ensayo')
-    bgcolor('gray')
-    setup(1000, 600, 0, 0)
-
-def draw_zone_one():
-    setworldcoordinates(500, 300, 300, 0)
-    fillcolor('blue')
-    begin_fill()
-    for i in range(4):
-        forward(100)
-        right(90)
-    end_fill()
-
-
-def calculate_angle_refraction(n_a, n_b, angle_a):
-    x = (n_a/n_b) * math.sin(degrees_to_radians(angle_a))
-    print('x ->', x)
-    return radians_to_degrees(math.asin(x))
-
-def degrees_to_radians(angle):
-    return ((angle * math.pi) / 180)
-
-def radians_to_degrees(angle):
-    return ((angle * 180) / math.pi)
-
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((700,500))
 
 if __name__ == '__main__':
-    draw()
-    draw_zone_one()
-    mainloop()
-
+    x = refraction_reflection(1.32, 1.52, 60)
+    print(x.angle_b)
+    # main()  
+    # while True:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             sys.exit()    
---
+++ refraction_reflection.py
@@ -0,0 +1,21 @@
+import math
+class refraction_reflection:
+
+    # n_a -> refractive index of material a
+    # n_b -> refractive index of material b
+    #angle_a -> angle of incidence 
+    def __init__(self, n_a, n_b, angle_a):
+        self.__n_a = n_a
+        self.__n_b = n_b
+        self.__angle_a = angle_a
+        self.angle_b = self.__calculate_angle_refraction()
+
+    def __calculate_angle_refraction(self):
+        x = (self.__n_a/self.__n_b) * math.sin(self.__degrees_to_radians(self.__angle_a))
+        return self.__radians_to_degrees(math.asin(x))
+
+    def __degrees_to_radians(self, angle):
+        return ((angle * math.pi) / 180)
+
+    def __radians_to_degrees(self, angle):
+        return ((angle * 180) / math.pi)
